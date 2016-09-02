2fedb905479b02d67493a14b1bb4546bde572d58
Petr Hosek <phosek@google.com>
2016-09-02 13:58:16 -0700

    Upload Magenta prebuilts together with sysroots
    Currently, only sysroots are building sent out as a change which
    causes the Magenta prebuilts to slip behind causing issues. With
    this change, the script will also uploads Magenta prebuilts and
    send a second change for updating these.
    
    Change-Id: I82bf788eef52b1c626f47c7ebd03d56d558899fc

--- scripts/build-sdk.sh
+++ scripts/build-sdk.sh
@@ -23,10 +23,14 @@ if [[ -z "${JIRI_ROOT}" ]]; then
   exit 1
 fi
 
-readonly GERRIT_OPTS="cc=fuchsia-reviews@google.com"
-
 readonly OUT_DIR="${JIRI_ROOT}/out"
-readonly SDK_URL="gs://fuchsia-build/fuchsia/sysroot"
+readonly SYSROOT_BUCKET="gs://fuchsia-build/fuchsia/sysroot"
+readonly MAGENTA_BUCKET="gs://fuchsia-build/magenta"
+
+readonly MAGENTA_HASH="$(git -C "${JIRI_ROOT}/magenta" rev-parse --short HEAD)"
+readonly MAGENTA_COMMIT="$(git -C "${JIRI_ROOT}/magenta" rev-parse HEAD)"
+
+readonly GERRIT_OPTS="topic=magenta/${MAGENTA_HASH},cc=fuchsia-reviews@google.com"
 
 # Build the sysroots
 export PATH="${JIRI_ROOT}/buildtools:${JIRI_ROOT}/buildtools/cmake/bin:${PATH}"
@@ -38,13 +42,43 @@ fi
 ${JIRI_ROOT}/.jiri_root/bin/toyen -src "${JIRI_ROOT}" -out "${OUT_DIR}" "${JIRI_ROOT}/packages/root.bp"
 ninja -C "${OUT_DIR}" -j1 sysroot
 
+readonly MAGENTA_BINARIES=("magenta.bin" "magenta.elf")
+readonly MAGENTA_TOOLS=("bootserver" "loglistener" "mkbootfs" "netruncmd")
+
+# Upload Magenta
+upload_magenta() {
+  local arch=$1
+  for file in ${MAGENTA_BINARIES[@]}; do
+    gsutil cp "${OUT_DIR}/build-magenta-${arch}/${file}" "${MAGENTA_BUCKET}/${arch}/${file}/${MAGENTA_COMMIT}"
+    echo "${MAGENTA_COMMIT}" > "${JIRI_ROOT}/packages/prebuilt/versions/magenta/${arch}/${file}"
+  done
+}
+
+upload_tools() {
+  local arch=$1
+  for file in ${MAGENTA_TOOLS[@]}; do
+    gsutil cp "${OUT_DIR}/build-magenta-${arch}/tools/${file}" "${MAGENTA_BUCKET}/tools/${file}/${MAGENTA_COMMIT}"
+    echo "${MAGENTA_COMMIT}" > "${JIRI_ROOT}/packages/prebuilt/versions/magenta/tools/${file}"
+  done
+}
+
+upload_magenta "qemu-arm64"
+upload_magenta "pc-x86-64"
+upload_tools "pc-x86-64"
+
+# Send a change
+if ! git -C "${JIRI_ROOT}/packages" diff-files --quiet; then
+  git -c "user.email=fuchsia.robot@gmail.com" -c "user.name=Fuchsia Robot" -C "${JIRI_ROOT}/packages" commit -m "Update Magenta prebuilts" -m "Updated to Magenta commit ${MAGENTA_HASH}." -- packages/prebuilt/versions/magenta/*
+  git -c "user.email=fuchsia.robot@gmail.com" -c "user.name=Fuchsia Robot" -C "${JIRI_ROOT}/packages" push origin HEAD:refs/for/master%${GERRIT_OPTS:-}
+fi
+
 # Upload the sysroots
 upload_sysroot() {
   local arch=$1
   local tarball="$(LC_ALL=POSIX cat $(find "${OUT_DIR}/sysroot/${arch}-fuchsia" -type f | sort) | shasum -a1  | awk '{print $1}')"
   tar -C ${OUT_DIR}/sysroot -jcvf "${tarball}" ${arch}-fuchsia
-  if ! gsutil stat "${SDK_URL}/${arch}/${tarball}" &>/dev/null; then
-    gsutil cp "${tarball}" "${SDK_URL}/${arch}/${tarball}"
+  if ! gsutil stat "${SYSROOT_BUCKET}/${arch}/${tarball}" &>/dev/null; then
+    gsutil cp "${tarball}" "${SYSROOT_BUCKET}/${arch}/${tarball}"
     echo "${tarball}" > "${JIRI_ROOT}/buildtools/sysroot/${arch}-fuchsia.sha1"
   fi
   rm -f "${tarball}"
@@ -53,11 +87,9 @@ upload_sysroot() {
 upload_sysroot "x86_64"
 upload_sysroot "aarch64"
 
-readonly MAGENTA_COMMIT="$(git -C "${JIRI_ROOT}/magenta" rev-parse --short HEAD)"
-
 # Send a change
 if ! git -C "${JIRI_ROOT}/buildtools" diff-files --quiet; then
-  git -c "user.email=fuchsia.robot@gmail.com" -c "user.name=Fuchsia Robot" -C "${JIRI_ROOT}/buildtools" commit -m "Update Fuchsia sysroots" -m "Updated to Magenta commit ${MAGENTA_COMMIT}." -- sysroot/x86_64-fuchsia.sha1 sysroot/aarch64-fuchsia.sha1
+  git -c "user.email=fuchsia.robot@gmail.com" -c "user.name=Fuchsia Robot" -C "${JIRI_ROOT}/buildtools" commit -m "Update Fuchsia sysroots" -m "Updated to Magenta commit ${MAGENTA_HASH}." -- sysroot/x86_64-fuchsia.sha1 sysroot/aarch64-fuchsia.sha1
   git -c "user.email=fuchsia.robot@gmail.com" -c "user.name=Fuchsia Robot" -C "${JIRI_ROOT}/buildtools" push origin HEAD:refs/for/master%${GERRIT_OPTS:-}
 fi
 
