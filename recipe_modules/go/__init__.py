93f28e24bef424b4442bfc180fd2270db2914d20
Petr Hosek <phosek@google.com>
2017-03-24 15:50:50 -0700

    Remove recipes
    These are no longer being used as all the recipes were consolidated
    into https://fuchsia.googlesource.com/infra/recipes/
    
    Change-Id: I993a4e50c8ce72476d9d0310cbe13fdcb810ffed

--- recipe_modules/go/__init__.py
+++
@@ -1,9 +0,0 @@
-DEPS = [
-    'cipd',
-    'recipe_engine/json',
-    'recipe_engine/path',
-    'recipe_engine/platform',
-    'recipe_engine/python',
-    'recipe_engine/raw_io',
-    'recipe_engine/step',
-]
